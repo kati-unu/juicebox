05ebfd5d83722bb20c49ab4ef982df8bc4863b47
Katiana Camacho-Villalon <katianajanea@gmail.com>
2020-09-17 01:40:13 -0700

    updated users table, created posts table and functions

--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,13 @@
 const {
     client,
     getAllUsers,
-    createUser
+    createUser,
+    updateUser,
+    getAllPosts,
+    createPost,
+    updatePost,
+    getPostsByUser,
+    getUserById
 } = require('./index');
 
 async function testDB() {
@@ -12,6 +18,32 @@ async function testDB() {
 
         const users = await getAllUsers();
         console.log("getAllUsers:", users);
+
+        console.log("Calling updateUser on users[0]")
+        const updateUserResult = await updateUser(users[0].id, {
+            name: "Newname Sogood",
+            location: "Lesterville, KY"
+        });
+        console.log("updateUserResult:", updateUserResult)
+
+        //call all posts
+        console.log("Calling getAllPosts");
+        const posts = await getAllPosts();
+        console.log("Result:", posts);
+
+        //updatepost
+        console.log("Calling updatePost on posts[0]");
+        console.log('POSTS[0].id:', posts[0].authorId ) //AUTHOR ID
+        const updatePostResult = await updatePost(posts[0].authorId, {
+          title: "New Title",
+          content: "Updated Content"
+        });
+        console.log("Result:", updatePostResult);
+
+        //getuserbyid 
+        console.log("Calling getUserById with 1");
+        const albert = await getUserById(1);
+        console.log("Result:", albert);
 
         console.log("Finished database tests!");
     } catch (error) {
@@ -24,7 +56,10 @@ async function dropTables() {
     try {
         console.log("Starting to drop tables...");
 
-        await client.query(`DROP TABLE IF EXISTS users;`);
+        await client.query(`
+        DROP TABLE IF EXISTS posts;
+        DROP TABLE IF EXISTS users;
+        `);
 
         console.log("Finished dropping tables!");
     } catch (error) {
@@ -41,9 +76,23 @@ async function createTables() {
         await client.query(`
             CREATE TABLE users (
             id SERIAL PRIMARY KEY,
-            username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL);
+            username VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
+            );
         `);
+
+        await client.query(`
+            CREATE TABLE posts (
+                id SERIAL PRIMARY KEY,
+                "authorId" INTEGER REFERENCES users(id) NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                content TEXT NOT NULL,
+                active BOOLEAN DEFAULT true
+            )
+        `)
 
         console.log("Finished building tables!");
     } catch (error) {
@@ -51,7 +100,7 @@ async function createTables() {
         throw error; 
     }
 }
-  
+
 async function rebuildDB() {
     try {
         client.connect();
@@ -59,7 +108,9 @@ async function rebuildDB() {
         await dropTables();
         await createTables();
         await createInitialUsers();
+        await createInitialPosts();
     } catch (error) {
+        console.log("Error during rebuildDB");
         throw error;
     }
 }
@@ -69,9 +120,9 @@ async function createInitialUsers() {
     try {
         console.log("Starting to create users...");
   
-        const albert = await createUser({ username: 'albert', password: 'bertie99' });
-        const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-        const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
+        const albert = await createUser({ username: 'albert', password: 'bertie99', name: 'Al Bert', location: 'Sidney, Australia' });
+        const sandra = await createUser({ username: 'sandra', password: '2sandy4me', name: 'Just Sandra', location: "Ain't tellin'" });
+        const glamgal = await createUser({ username: 'glamgal', password: 'soglam', name: 'Joshua', location: 'Upper East Side' });
         
         console.log(albert);
         console.log(sandra);
@@ -83,6 +134,34 @@ async function createInitialUsers() {
         throw error;
     }
 }
+
+async function createInitialPosts() {
+    try {
+      const [albert, sandra, glamgal] = await getAllUsers();
+  
+      await createPost({
+        authorId: albert.id,
+        title: "First Post",
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+      });
+
+      await createPost({
+        authorId: sandra.id,
+        title: "First Post2",
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+      });
+
+      await createPost({
+        authorId: glamgal.id,
+        title: "First Post3",
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+      });
+
+    } catch (error) {
+        console.log('Creating Post Error!')
+        throw error;
+    }
+  }
 
 
 rebuildDB()
